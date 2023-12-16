e1aa42d3184b6d7acbc430d28c89fe6ba80b6664
Jisooyanim <vladzzzzzss@gmail.com>
2023-12-16 22:26:09 +0800

    A: Background

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { React, useState } from "react";
+import Background from "./containers/Background";
+// import Palette from "./assets/Palette.jpg";
+import Gradient from "./assets/Gradient.jpg";
+import Navbar from "./components/Navbar"
 import "./App.css";
 
 function App() {
-
   return (
-    <div>
-      <p> Hello </p>
-    </div>
-  )
+    <Background bgImg={Gradient} position="left" blur={["bl", "tr"]}>
+      <Navbar/>
+    </Background>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+
+const Sidebar = () => {
+    return (
+        <p> Test</p>
+    );
+};
+
+export default Sidebar;
