1120dfc71e125049ee684deba9c9893f6c8bf565
Jisooyanim <vladzzzzzss@gmail.com>
2023-12-18 00:21:44 +0800

    M: Background - Image to Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { React, useState } from "react";
 import Background from "./containers/Background";
-// import Palette from "./assets/Palette.jpg";
-import Gradient from "./assets/Gradient.jpg";
 import Navbar from "./components/Navbar"
 import "./App.css";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <Background bgImg={Gradient} position="left" blur={["bl", "tr"]}>
-      <Navbar/>
+    <Background bgColor = "#4DA8DA">
+      <Container>
+        <p>Test</p>
+      </Container>
     </Background>
   );
 }
--- src/containers/Background.jsx
+++ src/containers/Background.jsx
@@ -1,66 +1,25 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
-const Background = ({ bgImg, position, offset, blur, className, children }) => {
-    const backgroundStyle = {
-        backgroundImage: `url(${bgImg})`,
+const Background = ({ bgColor, children }) => {
+    const backgroundStyles = {
+        backgroundColor: `${bgColor}`,
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
-        backgroundPosition: `${position}`,
-        width: "100vw",
-        height: "100vh",
+        position: "absolute",
+        zIndex: 100,
+        height: "100%",
+        width: "100%",
         margin: 0,
         overflow: "hidden",
         top: 0,
         left: 0,
-        position: "fixed",  
     };
 
-    // To make image full size, reset the margin and padding
-    document.body.style.margin = "0";
-    document.body.style.padding = "0";
-    document.documentElement.style.margin = "0";
-    document.documentElement.style.padding = "0";
-
-    const renderBlur = blur?.map((value, index) => {
-        let style = {};
-        const newOffset = offset ? `${offset}em` : "-25em";
-
-        switch (value) {
-            case "tr":
-                style = { ...style, top: newOffset, right: newOffset };
-                break;
-            case "bl":
-                style = { ...style, bottom: newOffset, left: newOffset };
-                break;
-            case "tl":
-                style = { ...style, top: newOffset, left: newOffset };
-                break;
-            case "br":
-                style = { ...style, bottom: newOffset, right: newOffset };
-                break;
-        }
-
-        return (
-            <React.Fragment key={index}>
-                <div className="radial-blur position-absolute" style={style} />
-            </React.Fragment>
-        );
-    });
-
     return (
-        <div style={backgroundStyle} className={className}>
-            <div
-                style={{
-                    position: "absolute",
-                    zIndex: 100,
-                    height: "100%",
-                    width: "100%",
-                }}
-            >
-                {children}
-            </div>
-            {renderBlur}
-        </div>
+        <Container fluid className="d-flex flex-column border" style={backgroundStyles}>
+            {children}
+        </Container>
     );
 };
 
