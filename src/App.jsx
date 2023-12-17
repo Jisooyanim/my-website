609702ca40a41c038cbc5351c6e47d708096ea0f
Jisooyanim <vladzzzzzss@gmail.com>
2023-12-18 01:16:36 +0800

    A: Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import { React, useState } from "react";
 import Background from "./containers/Background";
 import Navbar from "./components/Navbar"
 import "./App.css";
-import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <Background bgColor = "#4DA8DA">
-      <Container>
-        <p>Test</p>
-      </Container>
+    <Background bgColor = "#DE354C">
+      <Navbar/>
     </Background>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,37 @@
 import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { fonts } from "../themes/fonts";
 
 const Sidebar = () => {
-    return (
-        <p> Test</p>
-    );
+  return (
+    <Navbar
+      expand="lg"
+      className="bg-body-tertiary"
+      bg="dark"
+      data-bs-theme="dark"
+      fixed="top"
+      style={{
+        fontFamily: fonts.raleway,
+        fontWeight: 300,
+        fontSize: 19
+      }}
+    >
+      <Container>
+        <Navbar.Brand href="#home">Charles</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto" style={{marginLeft: "auto"}}> 
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#link">Skills</Nav.Link>
+            <Nav.Link href="#link">Projects</Nav.Link>
+            <Nav.Link href="#link">Timeline</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+            <Nav.Link href="#link">Resume</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Sidebar;
