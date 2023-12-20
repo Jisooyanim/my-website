61fdbf111e0e9b13ccfbd8b58802cb9996c438bc
Jisooyanim <vladzzzzzss@gmail.com>
2023-12-21 01:06:42 +0800

    M: Import fonts

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@fontsource/raleway";
+import "@fontsource/lexend-deca";
+import "@fontsource/ibm-plex-sans-devanagari";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
